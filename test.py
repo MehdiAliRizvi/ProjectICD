e64a3cf57ebbc46a166101890d81e9e61c0a0b35
Muntazir Ali <122056705+muntazir868@users.noreply.github.com>
2024-08-30 09:29:27 +0500

    operator is incoperated with a potential error of values stored mismatached

---
+++ test.py
@@ -0,0 +1,40 @@
+{
+    "ige": {
+        "names": [
+            "Quantitative immunoglobulin E measurement",
+            "Immunoglobulin E measurement"
+        ],
+        "units": [
+            "iu/ml",
+            "ku/l"
+        ]
+    },
+    "CO2": {
+        "names": [
+            "Carbon dioxide measurement",
+            "Serum or plasma carbon dioxide measurement (moles/volume)"
+        ],
+        "units": [
+            "mmol/l",
+            "meq/l",
+            null
+        ]
+    },
+    "blood_pH": {
+        "names": [
+            "pH measurement"
+        ],
+        "units": [
+            null,
+            "NA",
+            "ph units",
+            "N/A",
+            "n/a",
+            "-",
+            "na",
+            "ph",
+            ".",
+            "units"
+        ]
+    },
+}
